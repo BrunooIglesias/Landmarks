78947ce13a7463645b66f2ff12f5ba4a7cb5798a
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2025-04-02 17:00:09 -0300

    Removed Xcode local config files and updated .gitignore

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Bruno Sebastian Silva Iglesias on 4/2/25.
+//
+
+import Foundation
---
+++ Landmarks/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Bruno Sebastian Silva Iglesias on 4/2/25.
+//
+
+import Foundation
