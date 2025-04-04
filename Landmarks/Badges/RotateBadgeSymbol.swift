51f8459af31af41674349dda3d7df1a7f2b7049b
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2025-04-04 14:26:11 -0300

    feat: badge created

---
+++ Landmarks/Badges/RotateBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Bruno Sebastian Silva Iglesias on 4/4/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
